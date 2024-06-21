e060ef6797cae09fb5afc601e561d5efed57ddf1
Imran Khan <99397606+imran120198@users.noreply.github.com>
2024-06-21 20:04:13 +0530

    Update Movie.model.js

--- Model/Movie.model.js
+++ Model/Movie.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const MovieSchema = new mongoose.Schema({
+  image: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
   releaseYear: { type: Number, required: true },
